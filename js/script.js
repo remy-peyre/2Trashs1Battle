9c6929bf41cddb5244af9b6292b9f5dab301de32
remy peyre <remy.peyre05@gmail.com>
2017-05-09 19:07:47 +0200

    NAV / CONTACT / FOOTER /CONCEPT / NEXT-BATTLE DONE

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
-$(document).ready(function () {
 
-    $(".menu-btn a").click(function () {
+$(document).ready(function () {
+    $(".menu-btn #open_close").click(function () {
         $(".overlay").fadeToggle(200);
         $(this).toggleClass('btn-open').toggleClass('btn-close');
+        $('body').css('overflow','hidden');
     });
-
     $('.overlay').on('click', function () {
         $(".overlay").fadeToggle(200);
-        $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
+        $(".menu-btn #open_close").toggleClass('btn-open').toggleClass('btn-close');
+        $('body').css('overflow','auto');
     });
-
-    $('.menu a').on('click', function () {
+    /*$('.menu a').on('click', function () {
         $(".overlay").fadeToggle(200);
         $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
-    });
+    });*/
 
+    var $input = $('.form-fieldset > input');
+
+    $input.blur(function (e) {
+        $(this).toggleClass('filled', !!$(this).val());
+    });
 });
+
+/*
+var $input = $('.form-fieldset > input');
+
+$input.blur(function (e) {
+    $(this).toggleClass('filled', !!$(this).val());
+});*/
